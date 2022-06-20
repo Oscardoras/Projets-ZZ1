fdd3574febc4e7a25716cb26f43ad75cb6c2bf56
Oscar Buon <oscar.buon@etu.isima.fr>
2022-06-20 15:36:34 +0200

    Background added.

--- main.c
+++ main.c
@@ -2,6 +2,11 @@
 #include <stdio.h>
 
 
+void draw(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 50, 0, 0, 255);
+    SDL_RenderClear(renderer);
+}
+
 int main() {
     int window_size = 300, window_x = 0, window_y = 0, mouse_x, mouse_y;
 
@@ -23,7 +28,22 @@ int main() {
         exit(EXIT_FAILURE);
     }
     
+    SDL_Renderer* renderer = SDL_CreateRenderer(
+        window,
+        -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
     
+    if (renderer == NULL) {
+        SDL_Log("Error : SDL renderer creation - %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    draw(renderer);
+    SDL_RenderPresent(renderer);
     SDL_bool running = SDL_TRUE;
     SDL_Event event;
     
@@ -52,12 +72,15 @@ int main() {
             SDL_GetWindowPosition(window, &window_x, &window_y);
             SDL_GetMouseState(&mouse_x, &mouse_y);
             SDL_SetWindowPosition(window, window_x + mouse_x-(window_size/2), window_y + mouse_y-(window_size/2));
+            draw(renderer);
         }
         
+        SDL_RenderPresent(renderer);
         SDL_Delay(10);
     }
     
     
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     
