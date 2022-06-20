1350af95197dc7f9dc5dad6df5180139db25a7dd
pachapuis <pachapuis@a208pc07L.local.isima.fr>
2022-06-20 18:21:59 +0200

    Correction de bug

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define SNAKE_COUNT 7
+#define SNAKE_COUNT 6
 #define SNAKE_SIZE_MIN 10
-#define SNAKE_SIZE_MAX 20
+#define SNAKE_SIZE_MAX 30
 #define DISTANCE_PER_TICK 20
 typedef struct snake
 {
@@ -15,14 +15,12 @@ typedef struct snake
 	float angle;
 	struct snake * next;
 	struct snake * before;
-	unsigned int X3;
-	unsigned int X2;
-	unsigned int X1;
+	unsigned int time;
 } snake_t;
 float toRad(unsigned int degres)
 {
 	degres = degres%360;
-	return (float)degres / 360.0 * 6.28;
+	return ((float)degres) / 360.0 * 6.28;
 }
 void updateSerpent(snake_t* snake_ptr)
 {
@@ -36,9 +34,13 @@ void updateSerpent(snake_t* snake_ptr)
 	}
 	else
 	{
-		snake_ptr->angle = (int)(pow(snake_ptr->X3*(time(0)%50), 3) + pow(snake_ptr->X2*(time(0)%50), 2) + pow(snake_ptr->X1*(time(0)%50), 1))%360;
 		snake_ptr->x = snake_ptr->x + cos(toRad(snake_ptr->angle)) * DISTANCE_PER_TICK;
 		snake_ptr->y = snake_ptr->y + sin(toRad(snake_ptr->angle)) * DISTANCE_PER_TICK;
+		if(snake_ptr->time != time(0))
+        	{
+                snake_ptr->time = time(0);
+                snake_ptr->angle = rand() % 360;
+        	}
 	}
 	snake_ptr->x = snake_ptr->x%800;
 	snake_ptr->y = snake_ptr->y%600;
@@ -53,9 +55,6 @@ void creationSerpent(snake_t** snake_ptr)
 		(*snake_ptr)->length = rand()%(SNAKE_SIZE_MAX - SNAKE_SIZE_MIN)+SNAKE_SIZE_MIN;
 		(*snake_ptr)->angle = (float)(rand()%360) / 360 * ((2.0*3.14));
 		(*snake_ptr)->before = 0;
-		(*snake_ptr)->X3 = rand()%10;
-                (*snake_ptr)->X2 = rand()%10;
-		(*snake_ptr)->X1 = rand()%10;
 	}
 	else
 		exit(EXIT_FAILURE);
@@ -84,7 +83,7 @@ void drawSnake(snake_t * snake_ptr, SDL_Renderer * renderer)
                 drawSnake(snake_ptr->next, renderer);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0,255);
 	float angle4 = toRad(snake_ptr->angle+10);
-	float angle3 = toRad(snake_ptr->angle-10);
+	float angle3 = toRad(snake_ptr->angle+350);
 	float angle1 = toRad(snake_ptr->angle+170);
         float angle2 = toRad(snake_ptr->angle+190);
 	SDL_RenderDrawLine(renderer,10*cos(angle1) + snake_ptr->x, 10*sin(angle1) + snake_ptr->y, 10*cos(angle2) + snake_ptr->x, 10*sin(angle2) + snake_ptr->y);
@@ -122,7 +121,7 @@ void eventWhile(SDL_Renderer* renderer)
 		}
 		draw(renderer, snakes);
         	SDL_RenderPresent(renderer);
-  		SDL_Delay(20);
+  		SDL_Delay(60);
 		for(unsigned int it = 0; it < SNAKE_COUNT; ++it)
 			updateSerpent(snakes[it]);
  	}
