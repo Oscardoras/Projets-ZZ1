842d445562f835f48376ccdccb229ea352190493
Oscar Buon <oscar.buon@etu.isima.fr>
2022-06-20 14:09:30 +0200

    X window done.

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 
 int main() {
+    int window_size = 300, window_x = 0, window_y = 0, mouse_x, mouse_y;
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_Log("Error : SDL initialisation - %s\n", SDL_GetError());
@@ -11,8 +12,8 @@ int main() {
     
     SDL_Window* window = SDL_CreateWindow(
         "Test de fenêtre",
-        0, 0,
-        400, 300,
+        window_x, window_y,
+        window_size, window_size,
         SDL_WINDOW_RESIZABLE
     );
     
@@ -22,7 +23,38 @@ int main() {
         exit(EXIT_FAILURE);
     }
     
-    SDL_Delay(2000);
+    
+    SDL_bool running = SDL_TRUE;
+    SDL_Event event;
+    
+    while (running) {
+        if (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                running = SDL_FALSE;
+                break;
+            case SDL_KEYDOWN:
+                if (event.key.keysym.sym == SDLK_ESCAPE)
+                    running = SDL_FALSE;
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                if (SDL_GetMouseState(NULL, NULL) && SDL_BUTTON(SDL_BUTTON_LEFT))
+                    window_size = 600;
+                else
+                    window_size = 300;
+                break;
+            default:
+                break;
+            }
+            
+            SDL_SetWindowSize(window, window_size, window_size);
+            SDL_GetWindowPosition(window, &window_x, &window_y);
+            SDL_GetMouseState(&mouse_x, &mouse_y);
+            SDL_SetWindowPosition(window, window_x + mouse_x-(window_size/2), window_y + mouse_y-(window_size/2));
+        }
+        
+        SDL_Delay(10);
+    }
     
     
     SDL_DestroyWindow(window);
