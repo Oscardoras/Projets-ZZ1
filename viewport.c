cb45c5c50fdc77ca01345dc84045ea8f3ccc7942
pachapuis <pachapuis@etu.uca.fr>
2022-06-21 15:13:59 +0200

    Redimensionnement

--- viewport.c
+++ viewport.c
@@ -5,53 +5,36 @@
 #include "world_update.h"
 
 
-Viewport* initViewport(World* world, int width, int height) {
-    Viewport* viewport = malloc(sizeof(Viewport));
-    if (viewport == NULL) {
-        SDL_Log("Error : viewport creation");
+Viewport *initViewport(World *world, unsigned int width, unsigned int height) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+    SDL_Log("Erreur initialisation SDL");
+    exit(EXIT_FAILURE);
+    }
+    Viewport* viewport = (Viewport*)malloc(sizeof(Viewport));
+    if(!viewport)
+    {
+        SDL_Log("Erreur creation viewport");
         exit(EXIT_FAILURE);
     }
-    
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("Error : SDL initialisation - %s", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-    
+    viewport->window = SDL_CreateWindow("Game of life",
+                                                  SDL_WINDOWPOS_CENTERED,
+                                                  SDL_WINDOWPOS_CENTERED, width,
+                                                  height,
+                                                  SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     viewport->world = world;
+    viewport->renderer = SDL_CreateRenderer(viewport->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     viewport->width = width;
     viewport->height = height;
-    
-    viewport->window = SDL_CreateWindow(
-        "Game of life",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        width, height,
-        SDL_WINDOW_RESIZABLE
-    );
-    if (viewport->window == NULL) {
-        SDL_Log("Error : SDL window creation - %s", SDL_GetError());
-        closeViewport(viewport);
-    }
-    
-    viewport->renderer = SDL_CreateRenderer(
-        viewport->window,
-        -1,
-        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
-    );
-    if (viewport->renderer == NULL) {
-        SDL_Log("Error : SDL renderer creation - %s", SDL_GetError());
-        closeViewport(viewport);
-    }
     
     return viewport;
 }
 
-void closeViewport(Viewport* viewport) {   
-    if (viewport != NULL) {
-        if (viewport->renderer != NULL) SDL_DestroyRenderer(viewport->renderer);
-        if (viewport->window != NULL) SDL_DestroyWindow(viewport->window);
-        SDL_Quit();
-        free(viewport);
-    }
+void closeViewport(Viewport *viewport) {   
+    SDL_DestroyRenderer(viewport->renderer);
+    SDL_DestroyWindow(viewport->window);
+    SDL_Quit();
+    free(viewport);
 }
 
 int configInit(Viewport* viewport) {
@@ -81,7 +64,15 @@ int configInit(Viewport* viewport) {
                         *cell = !(*cell);
                     }
                     break;
-                
+                case SDL_WINDOWEVENT:
+                    if(event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+                        SDL_Log("resized");
+                        viewport->width = event.window.data1;
+                        viewport->height = event.window.data2;
+                        drawCells(viewport);
+                        }
+
+                break;
                 case SDL_KEYDOWN:
                     switch (event.key.keysym.sym) {
                         case SDLK_RETURN:
@@ -111,76 +102,72 @@ int configInit(Viewport* viewport) {
     return quitState;
 }
 
-void eventLoop(Viewport* viewport, int delay) {
+void eventLoop(Viewport *viewport, int *delay) {
     SDL_Event event;
-    bool running = true;
+    bool continuer = true;
     bool modified = true;
-    while (running) {
+    while (continuer) {
         while(SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
-                    running = false;
+                    continuer = false;
                     break;
-                case SDL_WINDOWEVENT_RESIZED:
-                    viewport->width = event.window.data1;
-                    viewport->height = event.window.data2;
-                    break;
+
+                case SDL_WINDOWEVENT:
+                    if(event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+                        SDL_Log("resized");
+                        viewport->width = event.window.data1;
+                        viewport->height = event.window.data2;
+                        drawCells(viewport);
+                        }
+                break;
 
                 case SDL_KEYDOWN:
                     switch(event.key.keysym.sym) {
                         case SDLK_RETURN:
-                            running = false;
+                            continuer = false;
                             break;
+
                         case SDLK_LEFT:
-                            delay = (delay > 10) ? delay-1 : 10;
+                            *delay = (*delay>10)?*delay-1:10;
                             break;
+
                         case SDLK_RIGHT:
-                            delay = (delay < 500) ? delay+1 : 500;
+                            *delay = (*delay<500)?*delay+1:500;
                             break;
                     }
                     break;
             }
         }
-
-        if (modified)
-            modified = update_world(viewport->world, "B3/S23");
+        modified = update_world(viewport->world, "B3/S23");
+        if (modified) drawCells(viewport);
+        SDL_Delay(*delay);
         
-        drawCells(viewport);
-        SDL_Delay(delay);
     }
 }
 
-void drawCells(Viewport* viewport) {
-    for(int x = 0; x < viewport->world->width; x++)
-        for (int y = 0; y < viewport->world->height; y++) {
-            if (*get_world_cell(viewport->world, x, y))
-                SDL_SetRenderDrawColor(viewport->renderer, 200, 200, 200, 255);
-            else
+void drawCells(Viewport *viewport) {
+    for(int x = 0; x < viewport->world->width; ++x)
+        for(int y = 0; y < viewport->world->height; ++y)
+        {
+            //if(!(*get_world_cell(viewport->world, x, y)))
+            if(rand()%2)
                 SDL_SetRenderDrawColor(viewport->renderer, 255, 255, 255, 255);
+            else
+                SDL_SetRenderDrawColor(viewport->renderer, 220, 220, 220, 255);
 
             SDL_Rect rect;
-            rect.w = viewport->width/viewport->world->width;
-            rect.h = viewport->height/viewport->world->height;
-            rect.x = x*rect.w;
-            rect.y = y*rect.h;
+            rect.w = (float)viewport->width/(float)viewport->world->width+1;
+            rect.h = (float)viewport->height/(float)viewport->world->height+1;
+            rect.x = x*(float)viewport->width/(float)viewport->world->width;
+            rect.y = y*(float)viewport->height/(float)viewport->world->height;
             SDL_RenderFillRect(viewport->renderer, &rect);
         }
-    
     SDL_SetRenderDrawColor(viewport->renderer, 100, 100, 100, 255);
-    for (int x = 0; x < viewport->world->width; x++)
-        SDL_RenderDrawLine(viewport->renderer,
-            x*(viewport->width/viewport->world->width),
-            0,
-            x*(viewport->width/viewport->world->width),
-            viewport->height
-        );
-    for (int y = 0; y < viewport->world->width; y++)
-        SDL_RenderDrawLine(viewport->renderer,
-            0,
-            y*(viewport->height/viewport->world->height),
-            viewport->width,
-            y*(viewport->height/viewport->world->height)
-        );
-    
+    for(int x = 1; x < viewport->world->width+1; ++x)
+        SDL_RenderDrawLine(viewport->renderer, x*((float)viewport->width/(float)viewport->world->width), 0, x*((float)viewport->width/(float)viewport->world->width), viewport->height);
+    for(int y = 1; y < viewport->world->height+1; ++y)
+        SDL_RenderDrawLine(viewport->renderer, 0, y*((float)viewport->height/(float)viewport->world->height), viewport->width, y*((float)viewport->height/(float)viewport->world->height));
+
     SDL_RenderPresent(viewport->renderer);
 }
