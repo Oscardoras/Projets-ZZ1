7998b603aa6521007251b9c6450e8aec27e2b181
pachapuis <pachapuis@etu.uca.fr>
2022-06-22 11:02:59 +0200

    Fichier source matrix

---
+++ src/matrix.c
@@ -0,0 +1,26 @@
+#include "matrix.h"
+
+matrix_t* init(FILE * file)
+{
+
+}
+
+void close(matrix_t &matrix)
+{
+
+}
+
+float* get(matrix_t &matrix, unsigned int i, unsigned int j)
+{
+
+}
+
+matrix_t copy(matrix_t &matrix)
+{
+
+}
+
+void forward(matrix_t &markov, unsigned int &currentState)
+{
+
+}
