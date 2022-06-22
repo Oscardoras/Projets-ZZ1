e5fda0b58b26e6d9d6314d829115fdcedf9b3aa4
pachapuis <pachapuis@etu.uca.fr>
2022-06-22 10:30:38 +0200

    Chargement des matrices à partir d'un fichier

--- src/matrix.h
+++ src/matrix.h
@@ -1,6 +1,6 @@
 #ifndef __MARKOV_H__
 #define __MARKOV_H__
-
+#include <stdio.h>
 typedef struct {
     float* data;
     unsigned int size;
@@ -9,10 +9,10 @@ typedef struct {
 /**
  * @brief create a new matrix
  * 
- * @param size le côté de la matrice
+ * @param fileName Le nom du fichier
  * @return La matrice créée
  */
-matrix_t init(unsigned int wsize);
+matrix_t init(FILE * file);
 
 /**
  * @brief Libère la mémoire en détruisant une matrice
