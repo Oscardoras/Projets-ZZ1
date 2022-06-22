f1a3f567605e8f4faff1c5bffea88fc31a3c8714
pachapuis <pachapuis@etu.uca.fr>
2022-06-22 10:56:36 +0200

    matrix.h complété

--- src/matrix.h
+++ src/matrix.h
@@ -1,6 +1,9 @@
 #ifndef __MARKOV_H__
 #define __MARKOV_H__
 #include <stdio.h>
+
+matrix_t *markov = nullptr;
+
 typedef struct {
     float* data;
     unsigned int size;
@@ -9,10 +12,10 @@ typedef struct {
 /**
  * @brief create a new matrix
  * 
- * @param fileName Le nom du fichier
+ * @param file Le fichier depuis lequel charger la matrice
  * @return La matrice créée
  */
-matrix_t init(FILE * file);
+matrix_t* init(FILE * file);
 
 /**
  * @brief Libère la mémoire en détruisant une matrice
