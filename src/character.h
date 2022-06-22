c0457f3dd3c59d237f0cee9e080ed30db60e32e4
maerhard <mathieu.erhard@etu.uca.fr>
2022-06-22 12:25:54 +0200

    Data Structures defined

--- src/character.h
+++ src/character.h
@@ -2,6 +2,34 @@
 #define __CHARACTER_H__
 
 
+typedef struct {
+    int x;
+    int y;
+    int rotation;
+} Position;
+
+typedef struct {
+    int hp;
+    int atk;
+    int atk_speed;
+    int speed;
+} InitStats;
+
+typedef struct {
+    InitStats stats;
+    matrix_t behaviour;
+    
+} ToE; //Type of entity
+
+typedef struct {
+    int current_hp;
+    int current_state;
+
+    ToE* type;
+
+    Position world_position;
+} Entity;
+
 
 
 
