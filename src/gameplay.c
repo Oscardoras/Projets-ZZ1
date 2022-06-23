127ad404ff05db96ea37f795619e0af28fb056c5
maerhard <mathieu.erhard@etu.uca.fr>
2022-06-23 16:33:34 +0200

    set_global_behaviour, add_target added

--- src/gameplay.c
+++ src/gameplay.c
@@ -3,3 +3,19 @@
 void set_global_behaviour(Level* level, GlobalBehavior behaviour) {
     level->states.behavior = behaviour;
 }
+
+void add_target(Level* level, int x, int y, Target target) {
+    Position pos;
+    pos.direction = 0;
+    pos.x = x;
+    pos.y = y;
+    
+    Entity* pheromone = new_entity(
+        search_type("Pheromone"),
+        target,
+        0,
+        pos
+    );
+    
+    add_level_entity(level, pheromone);
+}
