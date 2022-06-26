09bd111b2a75c2e48dbb3004a9e313d64e6f456c
Mathieu <erhard.mathieu@gmail.com>
2022-06-27 00:35:02 +0200

    new anim, no parallax moving, pulling new .png

--- anim.c
+++ anim.c
@@ -1,46 +1,23 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-void draw(SDL_Texture* texture, SDL_Window *window,
-                         SDL_Renderer *renderer, int pos) {
-  SDL_Rect 
-          source = {0},                         
-          window_dimensions = {0},              
-          destination = {0};                    
+#define ZOOM_F 5
+#define ZOOM_A 22.5
+#define ZOOM_M 22.5
 
-  SDL_GetWindowSize(
-      window, &window_dimensions.w,
-      &window_dimensions.h);                    
-  SDL_QueryTexture(texture, NULL, NULL,
-                   &source.w, &source.h);       
-    
-  int zoom = 5;
-  destination.w = source.w * zoom;
-  destination.h = source.h * zoom;
-  destination.y = 
-      (window_dimensions.h - destination.h) / 2;
-  destination.x = (pos)%(window_dimensions.w + destination.w) - destination.w;
-
-  SDL_SetRenderDrawColor(renderer, 220, 220, 220, 255);
-  SDL_RenderClear(renderer);
-  
-  SDL_RenderCopy(renderer, texture,
-                 &source,
-                 &destination);           
-  SDL_RenderPresent(renderer);               
-}
+SDL_Rect fourmiAnim[4];
 
 int main() {
     if (SDL_Init(SDL_INIT_VIDEO)) {
-    SDL_Log("Erreur init - %s\n", SDL_GetError());
+    SDL_Log("Erreur SDL init - %s\n", SDL_GetError());
     exit(EXIT_FAILURE);
     }
     
     SDL_Window* window = SDL_CreateWindow("Anim",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        740,
-        240,
+        720,
+        720,
         SDL_WINDOW_RESIZABLE
     );
     
@@ -54,29 +31,62 @@ int main() {
         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
     );
     
-    SDL_Texture* texture1 = NULL;
-    SDL_Texture* texture2 = NULL;
-    SDL_Texture* texture3 = NULL;
+    //Textures
+    SDL_Texture* textureFourmi = IMG_LoadTexture(renderer, "FourmiParallax.png");
+    SDL_Texture* textureArbre = IMG_LoadTexture(renderer, "ParallaxArbre.png");
+    SDL_Texture* textureMontagne = IMG_LoadTexture(renderer, "ParallaxMont.png");
     
-    SDL_Texture* tab_textures[4] = {texture1, texture2, texture3};
+
+    SDL_Rect
+        source_f = {0},
+        source_a = {0},
+        source_m = {0},                    
+        window_dimensions = {0},              
+        destination_f = {0},
+        destination_a = {0},
+        destination_m = {0},
+        fourmiAnim[4];
+
+
+    SDL_GetWindowSize(
+      window, &window_dimensions.w,
+      &window_dimensions.h);
     
-    char nom[10] = "f0.png";
+    //Dimensions des spritesheets
+    SDL_QueryTexture(textureFourmi, NULL, NULL,
+                   &source_f.w, &source_f.h);   
+    SDL_QueryTexture(textureArbre, NULL, NULL,
+                   &source_a.w, &source_a.h);
+    SDL_QueryTexture(textureMontagne, NULL, NULL,
+                   &source_m.w, &source_m.h);
     
-    for(int i=0; i<3; i++) {
-        nom[1] = '1' + i;
-        tab_textures[i] = IMG_LoadTexture(renderer, nom);
-        if(!tab_textures[i]) {
-            SDL_Log("Problem texture - %s", SDL_GetError());
-            for(int j=0; j<i; j++) SDL_DestroyTexture(tab_textures[j]);
-            SDL_DestroyRenderer(renderer);
-            SDL_DestroyWindow(window);
-            exit(EXIT_FAILURE);
-        }
+
+    //Emplacement de chaque sprite de fourmi
+    for(int i=0; i<4; i++) {
+        fourmiAnim[i].x = 0;
+        fourmiAnim[i].y = i*10;
+        fourmiAnim[i].w = 36;
+        fourmiAnim[i].h = 10;
     }
-    
-    tab_textures[3] = tab_textures[1];
-    
-    
+
+
+    //Emplacement des images sur l'écran
+    destination_f.w = source_f.w * ZOOM_F;
+    destination_f.h = source_f.h / 4 * ZOOM_F;
+    destination_f.x = (window_dimensions.w - destination_f.w) / 2;
+    destination_f.y = (window_dimensions.h - destination_f.h) / 1.1;
+
+    destination_a.w = source_a.h * ZOOM_A;
+    destination_a.h = source_a.h * ZOOM_A;
+    destination_a.x = 0;
+    destination_a.y = 0;
+
+    destination_m.w = source_m.h * ZOOM_M;
+    destination_m.h = source_m.h * ZOOM_M / 1.25;
+    destination_m.x = 0;
+    destination_m.y = -100;
+
+        
     int quit = 0;
     int frame = 0;
     int pos = 0;
@@ -88,11 +98,17 @@ int main() {
                 quit = 1;
         }
         
-        draw(tab_textures[frame], window, renderer, pos);
-        
-        frame = (frame+1)%4;
-        pos = (pos<0) ? 720+37*5 : pos-30;
-        
+        SDL_SetRenderDrawColor(renderer, 0, 191, 255, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderCopy(renderer, textureMontagne, &source_m, &destination_m);
+        SDL_RenderCopy(renderer, textureArbre, &source_a, &destination_a);
+        SDL_RenderCopy(renderer, textureFourmi, &fourmiAnim[frame], &destination_f);
+        SDL_RenderPresent(renderer);
         SDL_Delay(200);
+
+        frame = (frame+1) % 4;
     }
+
+    return 0;
 }
