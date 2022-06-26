30b82471474fc121f775f02332d6b00d587d4dfc
Mathieu <erhard.mathieu@gmail.com>
2022-06-27 01:22:46 +0200

    animation finished

--- anim.c
+++ anim.c
@@ -2,8 +2,10 @@
 #include <SDL2/SDL_image.h>
 
 #define ZOOM_F 5
-#define ZOOM_A 22.5
-#define ZOOM_M 22.5
+
+#define DECAL_A 3
+
+#define DECAL_M 1
 
 SDL_Rect fourmiAnim[4];
 
@@ -33,8 +35,8 @@ int main() {
     
     //Textures
     SDL_Texture* textureFourmi = IMG_LoadTexture(renderer, "FourmiParallax.png");
-    SDL_Texture* textureArbre = IMG_LoadTexture(renderer, "ParallaxArbre.png");
-    SDL_Texture* textureMontagne = IMG_LoadTexture(renderer, "ParallaxMont.png");
+    SDL_Texture* textureArbre = IMG_LoadTexture(renderer, "ParallaxArbre1.png");
+    SDL_Texture* textureMontagne = IMG_LoadTexture(renderer, "ParallaxMont1.png");
     
 
     SDL_Rect
@@ -76,20 +78,21 @@ int main() {
     destination_f.x = (window_dimensions.w - destination_f.w) / 2;
     destination_f.y = (window_dimensions.h - destination_f.h) / 1.1;
 
-    destination_a.w = source_a.h * ZOOM_A;
-    destination_a.h = source_a.h * ZOOM_A;
-    destination_a.x = 0;
+    destination_a.w = window_dimensions.w * 5/2;
+    destination_a.h = window_dimensions.h;
+    destination_a.x = -(destination_a.w - window_dimensions.w) * 2/3;
     destination_a.y = 0;
 
-    destination_m.w = source_m.h * ZOOM_M;
-    destination_m.h = source_m.h * ZOOM_M / 1.25;
-    destination_m.x = 0;
+    destination_m.w = window_dimensions.w * 5/2;
+    destination_m.h = window_dimensions.h * 0.8;
+    destination_m.x = -(destination_m.w - window_dimensions.w) * 2/3;
     destination_m.y = -100;
 
         
     int quit = 0;
     int frame = 0;
-    int pos = 0;
+    int eps_a = 0;
+    int eps_m = 0;
     SDL_Event event;
     
     while(!quit) {
@@ -103,12 +106,25 @@ int main() {
 
         SDL_RenderCopy(renderer, textureMontagne, &source_m, &destination_m);
         SDL_RenderCopy(renderer, textureArbre, &source_a, &destination_a);
-        SDL_RenderCopy(renderer, textureFourmi, &fourmiAnim[frame], &destination_f);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(200);
+        SDL_RenderCopy(renderer, textureFourmi, &fourmiAnim[frame/25], &destination_f);
 
-        frame = (frame+1) % 4;
+        SDL_RenderPresent(renderer);
+
+        frame = (frame+1) % 100;
+
+        eps_a = destination_a.x + DECAL_A;
+        destination_a.x = (eps_a > 0) ?
+            -(destination_a.w - window_dimensions.w) * 2/3 + eps_a :
+            eps_a;
+
+        eps_m = destination_m.x + DECAL_M;
+        destination_m.x = (eps_m > 0) ?
+            -(destination_m.w - window_dimensions.w) + eps_m :
+            eps_m;
+
+        SDL_Delay(10);
     }
+    
 
     return 0;
 }
